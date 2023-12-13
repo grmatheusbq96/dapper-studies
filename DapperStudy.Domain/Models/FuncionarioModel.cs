37262f80989a2572dfebf5e10fd4d6c63fe0012a
Matheus Rodrigues <matheus.rodrigues@havan.com.br>
2023-12-13 08:41:23 -0300

    Adicionado repositorio generico

---
+++ DapperStudy.Domain/Models/FuncionarioModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DapperStudy.Domain.Models
+{
+    public class FuncionarioModel
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public decimal Salario { get; set; }
+    }
+}
