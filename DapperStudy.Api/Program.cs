bfd2233c825f1c5249ed76610d4ab1b5d676cb20
Matheus Rodrigues <matheus.rodrigues@havan.com.br>
2023-12-14 10:05:17 -0300

    ConnectionString salva em appsettings

--- DapperStudy.Api/Program.cs
+++ DapperStudy.Api/Program.cs
@@ -1,24 +1,32 @@
 using DapperStudy.Infra.Persistence.Extensions;
 using DapperStudy.Services.Extensions;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace DapperStudy.Api;
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddServicesExtensions();
-builder.Services.AddPersistenceExtensions();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddServicesExtensions();
+        builder.Services.AddPersistenceExtensions();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-
-app.Run();
